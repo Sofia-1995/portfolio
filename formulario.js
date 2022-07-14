db18b528848acc86ef4cea73b4ccb087b6c9aedc
Sofia <sm.pereyra95@hotmail.com>
2022-07-14 15:45:38 -0300

    Valido campos de asunto, mensaje y boton enviar

--- formulario.js
+++ formulario.js
@@ -2,11 +2,15 @@ const form = document.getElementById("my-form");
 const formStatus = document.getElementById("my-form-status");
 const formStatusNombre = document.getElementById("my-form-status-nombre");
 const formStatusEmail = document.getElementById("my-form-status-email");
+const formStatusAsunto = document.getElementById("my-form-status-asunto");
+const formStatusMensaje = document.getElementById("my-form-status-mensaje");
 
 const mensajes = {
-  OK: "Gracias por contactarme!",
-  ERROR: "Lo siento, hubo un error al enviar el formulario",
-  NOMBRE_LARGO: "mensaje nombre largo",
+  OK: "Mensaje enviado. Gracias por contactarme!",
+  ERROR:
+    "Lo siento, hubo un error al enviar el formulario. Intente nuevamente más tarde.",
+  MAX: "Máximo 50 caracteres",
+  MAX_LARGO: "Máximo 300 caracteres",
   VACIO: "Este campo es obligatorio",
   NO_COINCIDE: "Este formato no coincide",
 };
@@ -19,7 +23,7 @@ const esVacio = (texto) => {
 
 const validarNombre = (nombre) => {
   if (nombre.length > 50) {
-    formStatusNombre.innerHTML = mensajes.NOMBRE_LARGO;
+    formStatusNombre.innerHTML = mensajes.MAX;
     return false;
   }
   if (esVacio(nombre)) {
@@ -41,20 +45,44 @@ const validarCorreo = (email) => {
   return true;
 };
 
-const validarAsunto = () => {};
+const validarAsunto = (asunto) => {
+  if (asunto.length > 50) {
+    formStatusAsunto.innerHTML = mensajes.MAX;
+    return false;
+  }
+  if (esVacio(asunto)) {
+    formStatusAsunto.innerHTML = mensajes.VACIO;
+    return false;
+  }
+  return true;
+};
 
-const validarMensaje = () => {};
+const validarMensaje = (mensaje) => {
+  if (mensaje.length > 300) {
+    formStatusMensaje.innerHTML = mensajes.MAX_LARGO;
+    return false;
+  }
+  if (esVacio(mensaje)) {
+    formStatusMensaje.innerHTML = mensajes.VACIO;
+    return false;
+  }
+  return true;
+};
 
 function handleSubmit(event) {
   event.preventDefault();
+  formStatusNombre.innerHTML = "";
+  formStatusAsunto.innerHTML = "";
+  formStatusEmail.innerHTML = "";
+  formStatusMensaje.innerHTML = "";
   const data = new FormData(event.target);
 
   const esNombreValido = validarNombre(data.get("nombre"));
   const esEmailValido = validarCorreo(data.get("email"));
+  const esAsuntoValido = validarAsunto(data.get("asunto"));
+  const esMensajeValido = validarMensaje(data.get("mensaje"));
 
-  if (esNombreValido && esEmailValido) {
-    console.log("todo ok, envio data");
-    return;
+  if (esNombreValido && esEmailValido && esAsuntoValido && esMensajeValido) {
     fetch("https://formspree.io/f/xeqnbwgv", {
       method: "POST",
       body: data,
